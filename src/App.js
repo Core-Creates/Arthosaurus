c56048db8ab4e0c86902c470df9698a0d45a4be5
Corrina Alcoser <corrinaalcoser@gmail.com>
2024-02-24 17:22:54 -0600

    added alexa speech to text to this react app

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import { Alexa } from 'ask-sdk-jsx-for-react';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div className="App">
+          <h1>My React App with Alexa Integration</h1>
+          <Alexa skillId="YOUR_SKILL_ID">
+            <Alexa.Intent name="LaunchRequest">
+              <Alexa.AddSpeechText>Hello! Welcome to my Alexa-enabled React app.</Alexa.AddSpeechText>
+            </Alexa.Intent>
+          </Alexa>
+        </div>
+
       </header>
     </div>
   );
