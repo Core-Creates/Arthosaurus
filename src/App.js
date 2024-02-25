e46cdb92d0359dffeef4b43df5ea9763b0b204dc
Corrina Alcoser <corrinaalcoser@gmail.com>
2024-02-24 18:10:09 -0600

    added alexa speech to text to this react app

--- src/App.js
+++ src/App.js
@@ -11,7 +11,10 @@ function App() {
           <h1>My React App with Alexa Integration</h1>
           <Alexa skillId="YOUR_SKILL_ID">
             <Alexa.Intent name="LaunchRequest">
-              <Alexa.AddSpeechText>Hello! Welcome to my Alexa-enabled React app.</Alexa.AddSpeechText>
+              <Alexa.AddSpeechText>Hello! Welcome to Arthosaurus, 
+                an Alexa-enabled React app to help individuals 
+                suffering with arthritis and other physical aliments. 
+              </Alexa.AddSpeechText>
             </Alexa.Intent>
           </Alexa>
         </div>
