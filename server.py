63e97069e6d01c42ca3c69583017ed29bccb7cbd
Corrina Alcoser <corrinaalcoser@gmail.com>
2024-02-25 08:18:42 -0600

    removed node modules

---
+++ server.py
@@ -0,0 +1,18 @@
+# This action requires the 'csv' module
+# import csv
+from flask import Flask, render_template
+app = Flask(__name__, instance_relative_config=True)
+
+@app.route('/')
+def index():
+  return render_template('index.html')
+
+@app.route('/alexa')
+def alexa_template():
+  print ('I got clicked!')
+
+  return render_template('alexa.html')
+
+if __name__ == '__main__':
+  app.run(debug=True)
+
